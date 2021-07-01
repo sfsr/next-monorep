35b98f49eff746d67daa488987a0cf2edb3380d0
Georgiy Antipov <g.antipov@app-smart.ru>
2021-07-01 15:40:08 +0300

    Added Vary header

--- packages/server/app.js
+++ packages/server/app.js
@@ -8,6 +8,7 @@ const buildUrl = (port) => `${process.env.URL || 'http://localhost'}:${port}`;
 createServer((req, res) => {
   const md = new MobileDetect(req.headers['user-agent']);
   const target = md.mobile() ? buildUrl('9009') : buildUrl('9008');
+  res.writeHead(200, { 'Vary': 'User-Agent', 'content-type': 'text/html; charset=utf-8', 'content-encoding': 'gzip' });
   proxy.web(req, res, {
     target: target,
   });
